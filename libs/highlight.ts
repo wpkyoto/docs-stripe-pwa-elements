7821bff73e84a8002c89b0ae9902a3adf2e9c860
Hidetaka Okamoto <info@wp-kyoto.net>
2021-09-20 22:35:28 +0900

    try: re-enable highlight.js

--- libs/highlight.ts
+++ libs/highlight.ts
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-/*
 import hljs from "highlight.js/lib/core";
+import "highlight.js/styles/atom-one-dark.css";
+/*
 import javascript from "highlight.js/lib/languages/javascript";
 import typescript from "highlight.js/lib/languages/typescript";
 import shell from "highlight.js/lib/languages/shell";
 import xml from "highlight.js/lib/languages/xml";
 import yaml from "highlight.js/lib/languages/yaml";
-import "highlight.js/styles/atom-one-dark.css";
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("typescript", typescript);
 hljs.registerLanguage("xml", xml);
@@ -20,7 +20,7 @@ hljs.registerLanguage("undefined", shell);
 export const useHighlightJS = () => {
   const { pathname } = useRouter();
   useEffect(() => {
-    // hljs.highlightAll();
+    hljs.highlightAll();
     //hljs.initHighlighting.called = false
   }, [pathname]);
 };
